f3949da6f5985fdfb3257dcd55fc01dbfb893a37
agyo <gyorgy.ali@invitel.hu>
2015-02-07 21:24:23 +0100

    Initial commit

---
+++ app/src/main/java/hu/itfactory/app/examples/appelsodemo/MyActivity.java
@@ -0,0 +1,43 @@
+package hu.itfactory.app.examples.appelsodemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Date;
+
+
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        Button btnShowDateTime = (Button) findViewById(R.id.btnShowDateTime);
+
+        btnShowDateTime.setOnLongClickListener( new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Toast.makeText( getApplicationContext(), "valami", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        btnShowDateTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        (CharSequence) getApplicationContext().getPackageCodePath(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+
+}
